82f1dbefa903777033067feda8781233f193a08c
J0sa3210 <joeri.winckelmans@outlook.be>
2024-02-18 20:14:10 +0100

    Created UDP conenction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,25 @@
 import java.io.IOException;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost",4000);
+            // Create the message
+            String message = "Hey Server!";
+            byte[] buf = message.getBytes();
+
+            // Get the ip address
+            InetAddress ip = InetAddress.getByName("127.0.0.1");
+
+            // Create the datagram
+            DatagramPacket p = new DatagramPacket(buf, 0, buf.length, ip,4000);
+
+            // Create a socket and send the datagrams
+            DatagramSocket datagramSocket = new DatagramSocket();
+            datagramSocket.send(p);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,15 +1,29 @@
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketAddress;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            System.out.println("Trying to connect to server");
-            ServerSocket ss = new ServerSocket(4000);
-            Socket socket = ss.accept();
-            System.out.println("Succesfully connected: " + socket.getLocalSocketAddress().toString());
+            // Create socket for UDP at port 4000
+            DatagramSocket datagramSocket = new DatagramSocket(4000);
+
+            // Create a buffer for the DatagramPacket to store the message
+            byte[] buf = new byte[1024];
+
+            // Create the datagramPacket and assign the size
+            DatagramPacket p = new DatagramPacket(buf, buf.length);
+
+            // Receive a message and put it in the DatagramPacket
+            datagramSocket.receive(p);
+
+            // Convert the bytes to string
+            String str = new String(p.getData(), 0, p.getLength());
+
+            // Print the message
+            System.out.println("Received packet: " + str);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
