380b6fb3ded5622b51c0a85f6958a4bc4a146af2
J0sa3210 <joeri.winckelmans@outlook.be>
2024-03-07 08:58:47 +0100

    Created app + 2 API lethods

--- src/main/java/com/example/lab2/BankAccount.java
+++ src/main/java/com/example/lab2/BankAccount.java
@@ -1,23 +1,30 @@
+package com.example.lab2;
+
 public class BankAccount {
     String nameHolder;
     Double balance;
 
-    public BankAccount(String nameHolder){
+    public BankAccount(String nameHolder) {
         this.nameHolder = nameHolder;
         this.balance = 0.;
     }
 
-    public void deposit(double amount){
+    public void deposit(double amount) {
         this.balance += amount;
     }
 
-    public void withdraw(double amount){
-        if (amount < this.balance){
+    public void withdraw(double amount) {
+        if (amount < this.balance) {
             this.balance -= amount;
         }
     }
 
-    public String showBalance(){
+    public String showBalance() {
         return String.valueOf(this.balance);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("BankAccount(holder: %s, balance: %s)", this.nameHolder, this.balance);
     }
 }
--- src/main/java/com/example/lab2/BankServer.java
+++ src/main/java/com/example/lab2/BankServer.java
@@ -1,2 +1,30 @@
-package com.example.lab2;public class BankServer {
+package com.example.lab2;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class BankServer {
+    Map<String,BankAccount> accounts = new HashMap<>();
+
+    @PostMapping("/openBankAccount")
+    String newBanckAccount(@RequestBody String name) {
+        BankAccount newAcc = new BankAccount(name);
+        accounts.put(name,newAcc);
+        return "Successfully created new account: " + newAcc.nameHolder;
+    }
+
+    @GetMapping("/getAccount")
+    String getAccount(@RequestParam(value = "name") String name) {
+        return accounts.get(name).toString();
+    }
+
+    @PostMapping("/depositMoney")
+    String depositMoney(@RequestParam(value="name") String name, @RequestBody double amount){
+        BankAccount acc = accounts.get(name);
+        acc.deposit(amount);
+        return "Successfully added balance to account: " + acc;
+    }
 }
